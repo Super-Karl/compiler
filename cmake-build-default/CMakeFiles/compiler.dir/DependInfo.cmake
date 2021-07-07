4c8df216719cd0df8f73940eb7960d576ae7db29
Super-Karl <754334015@qq.com>
2021-07-07 18:38:12 +0800

    添加公开测试样例

---
+++ cmake-build-default/CMakeFiles/compiler.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/karl/CLionProjects/compiler/src/controller/controller.cpp" "/mnt/c/Users/karl/CLionProjects/compiler/cmake-build-default/CMakeFiles/compiler.dir/src/controller/controller.cpp.o"
+  "/mnt/c/Users/karl/CLionProjects/compiler/src/entry.cpp" "/mnt/c/Users/karl/CLionProjects/compiler/cmake-build-default/CMakeFiles/compiler.dir/src/entry.cpp.o"
+  "/mnt/c/Users/karl/CLionProjects/compiler/src/front/ast/AstNode.cpp" "/mnt/c/Users/karl/CLionProjects/compiler/cmake-build-default/CMakeFiles/compiler.dir/src/front/ast/AstNode.cpp.o"
+  "/mnt/c/Users/karl/CLionProjects/compiler/src/mid/ir/ir.cpp" "/mnt/c/Users/karl/CLionProjects/compiler/cmake-build-default/CMakeFiles/compiler.dir/src/mid/ir/ir.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "front"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

84044ab857c5ce8d775179c469c087aedec5efa3
wnx233 <64090127+wnx233@users.noreply.github.com>
2021-07-07 12:00:46 +0800

    ir生成

--- src/mid/recordTable/RecordTable.cpp
+++ src/mid/recordTable/RecordTable.cpp
@@ -7,29 +7,30 @@
 #include <vector>
 
 namespace compiler::mid::ir {
-  VarInfo::VarInfo(std::string name,std::vector<int>& inShape,std::vector<int>& inValue,bool isConst = false){
+  VarInfo::VarInfo(std::string name, std::vector<int> &inShape, std::vector<int> &inValue, bool isConst = false) {
     this->name = name;
     this->shape = inShape;
     this->value = inValue;
     this->isConst = isConst;
     this->isArray = true;
   }
-  VarInfo::VarInfo(std::string name, int value, bool isConst = false) 
-    : name(name),isConst(isConst),isArray(false) {
-      this->value={value};
-  }
-  VarInfo::VarInfo(std::string name,std::vector<int>& inValue,std::vector<int>& inShape,bool isConst = false){
-
-
+  VarInfo::VarInfo(std::string name, int value, bool isConst = false)
+      : name(name), isConst(isConst), isArray(false) {
+    this->value = {value};
   }
 
-  RecordTable &RecordTable::insertVar(std::string name, VarInfo v) {
-    varTable[name] = v;
+  VarInfo::VarInfo(std::string name, std::vector<int> &inValue, std::initializer_list<int> inShape, bool isConst) {
   }
-  VarInfo &RecordTable::searchVar(std::string name) {
+
+  void RecordTable::insertVar(std::string name, VarInfo *v) {
+    varTable.insert({name, v});
+  }
+
+  VarInfo *RecordTable::searchVar(std::string name) {
     auto tmp = varTable.find(name);
     if (tmp != varTable.end())
       return varTable[name];//
     throw std::out_of_range("no var name " + name);
   }
+
 }// namespace compiler::mid::ir
--- src/mid/recordTable/RecordTable.h
+++ src/mid/recordTable/RecordTable.h
@@ -13,13 +13,13 @@ namespace compiler::mid::ir {
   public:
     bool isConst;
     bool isArray;
-    std::vector<int> value;
+    std::vector<int> value;//存储数据,如果是int,只有一个元素,如果是数组,数据个数为数组转化为一维之后的长度
     std::vector<int> shape;
     std::string name;//with identifier @,% 为ssa中rename后的名字
 
     VarInfo(std::string name, int value, bool isConst = false);
-    VarInfo(std::string name,std::vector<int>& inShape,std::vector<int>& inValue,bool isConst = false) ;
-    VarInfo::VarInfo(std::string name,std::vector<int>& inValue,std::initializer_list<int> inShape,bool isConst = false);
+    VarInfo(std::string name, std::vector<int> &inShape, std::vector<int> &inValue, bool isConst = false);
+    VarInfo(std::string name, std::vector<int> &inValue, std::initializer_list<int> inShape, bool isConst = false);
     //可用{}将形状框起传参
   };
 
@@ -27,15 +27,16 @@ namespace compiler::mid::ir {
   /* TODO 没实现φ函数,多分支情况下的ssa变量选取*/
   class RecordTable {
   private:
-    std::unordered_map<std::string, VarInfo> varTable;//符号表,变量的vec只有一个值,数组的vector会存储所有数组的值
+    std::unordered_map<std::string, VarInfo *> varTable;//符号表,变量的vec只有一个值,数组的vector会存储所有数组的值
     RecordTable *father;
     unsigned int id = 0;
 
   public:
     RecordTable(RecordTable *rt = NULL) : father(rt) {}
-    VarInfo &searchVar(std::string name);             //输入参数为变量名,返回在hash表中的引用
-    RecordTable &insertVar(std::string name, VarInfo);//插入单个varInfo元素
+    VarInfo *searchVar(std::string name);              //输入参数为变量名,返回在hash表中的引用
+    void insertVar(std::string name, VarInfo *varInfo);//插入单个varInfo元素
     unsigned int getID() { return this->id++; }
+    RecordTable *getFarther() { return father; }
   };
 }// namespace compiler::mid::ir
 

63c9bde2f6860c4a7519bc92a209e636a3f1f9d9
zakilim <zzqandlth@outlook.com>
2021-07-31 19:07:32 +0800

    add help option

--- testTools/autotest.sh
+++ testTools/autotest.sh
@@ -1,5 +1,11 @@
 rm  test.log
-if test $1 == "-a"
+if test $1 == "-h"
+then
+    echo "-a: 全测试"
+    echo "-s [order]: 单样例测试"
+    echo "-h 查看帮助"
+
+elif test $1 == "-a"
     then
     set i = 0
     for file in ../testcase/*.sy
@@ -31,5 +37,11 @@ elif test $1 = "-s"
     ./Exe
     echo $? > this.out
     #echo "filename ${file##*/%.*}"
-    diff this.out  ${file%.sy}.out >> test.log
+    diff -b -q this.out  ${file%.sy}.out >> test.log
+    if test $? -eq 0
+    then
+        echo "test $file failed" >> test.log
+    else
+        echo "test $file passed" >> test.log
+    fi
 fi

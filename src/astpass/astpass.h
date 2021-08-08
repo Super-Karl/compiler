79bff05bf94864b6e6a1fc17ac1c2f7267ebb135
Super-Karl <754334015@qq.com>
2021-08-08 19:54:59 +0800

    提交测试123

--- src/astpass/astpass.h
+++ src/astpass/astpass.h
@@ -25,8 +25,6 @@ namespace compiler::astpassir {
 
     compiler::front::ast::Expression *FirstPassExpr(compiler::front::ast::Expression *expr, Hash constTbale);
 
-    compiler::front::ast::Expression *FirstPassExprTop(compiler::front::ast::Expression *expr, Hash constTbale, Hash topDec);
-
     vector<compiler::front::ast::Expression *> FirstPassArrayLinelize(int sizeIndex, vector<compiler::front::ast::Expression *> &index, vector<compiler::front::ast::Expression *> &values);
 }
 

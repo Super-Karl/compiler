2f7e05e0d377920b7b822c692c11e53ff75e7c2f
duhaoai <994439420@qq.com>
2021-08-11 20:27:04 +0800

    修bug

--- src/entry.cpp
+++ src/entry.cpp
@@ -15,23 +15,5 @@ using namespace std;
 
 using Hash = std::unordered_map<string, int>;
 int main(int argc, char **argv) {
-    string inputfile=argv[4];
-    FILE *input =fopen(argv[4],"r");
-    if(input==nullptr){
-        cout<<"输出文件错误";
-    }
-  auto *root = compiler::controller::generator::generate(input);
-  Hash constTbale;
-  compiler::astpassir::FirstPassRoot(root, constTbale);
 
-  auto ir = compiler::controller::generator::genIR(root);
-  //compiler::controller::generator::printIR(ir);
-  auto arm = compiler::back::genarm::genBack(ir);
-    ofstream outfile;
-    string outputfile=argv[3];
-    outfile.open(outputfile);
-  for(auto var:arm){
-      outfile<<var->print();
-  }
-  return 0;
 }
